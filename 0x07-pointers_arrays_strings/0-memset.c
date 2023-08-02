2c6a6bf8c0d7dbdeb01217535cc41de5a6cc77d4
Aviwesonamzi <aviwesonamzi1@gmail.com>
2023-08-02 13:11:23 -0700

    memcpy

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,8 +1,11 @@
 #include"main.h"
-/*
- * _memset - fills memmory with a constant byte
+/**
+ ** _memset - fills memmory with a constant byte
+ * @s: starting address of memory to be filled
+ * @b: the desired value
+ * @n: number of bytes to be changed
  *
- * return : memmory b
+ * return: memmory s
  */
 char *_memset(char *s, char b, unsigned int n)
 {
