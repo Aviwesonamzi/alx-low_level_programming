959900aa06982c8f3071ba1637cf9d27e82e4185
Aviwesonamzi <aviwesonamzi1@gmail.com>
2023-08-03 18:28:15 -0700

    She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -9,10 +9,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i
+	unsigned int i = 0;
+
+	while (i < n)
 	{
-		while (i < n);
-		i++;
+		dest[i] = src[i];
 	}
 	return (dest);
 }
