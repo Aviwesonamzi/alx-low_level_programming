8af5f207bc30e7c70e9a73db64021e48178ea421
Aviwesonamzi <aviwesonamzi1@gmail.com>
2023-08-25 01:02:16 -0700

    Print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,13 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_H
+#define _LISTS_H
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
