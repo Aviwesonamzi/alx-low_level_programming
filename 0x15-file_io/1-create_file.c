5097be368511b7cb31085e02e10b6f3d67543e50
Aviwesonamzi <aviwesonamzi1@gmail.com>
2023-09-05 13:22:09 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -6,7 +6,7 @@
  *
  * Return: 1 on success
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
 	int fn, a, min = 0;
 
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content);
 
 	if (text_content != NULL)
 	{
-		for (len = 0; text_content[min];)
+		for (min = 0; text_content[min];)
 			min++;
 	}
 
